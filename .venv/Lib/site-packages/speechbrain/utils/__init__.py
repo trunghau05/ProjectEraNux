cc08ca3528873f7913f7607525e1a7aa807150d0
haunguyen05-dev <n.t.hau005@gmail.com>
2026-04-21 08:15:36 +0700

    move .venv to stt_server 2

--- .venv/Lib/site-packages/speechbrain/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Package containing various tools (accuracy, checkpoints ...)
-"""
-
-from speechbrain.utils.importutils import lazy_export_all
-
-lazy_export_all(__file__, __name__)
-
-from speechbrain.utils.seed import seed_everything  # noqa
